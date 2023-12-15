0ebd98d4d07eae30060422200d952602deae2b9e
Mark Kelsall <99473523+geekissh@users.noreply.github.com>
2023-12-15 09:27:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function razzle() {
+    console.log("You've been razzled!");
+  }
+  
+  razzle();
+  
+  function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+
+  function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective('!!!');
+  console.log(encouragingPromptFunction('a dedicated programmer'));
+  
